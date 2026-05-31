03ec90395b07b9b2469fa9be667cfafb33caa2f3
wdk59 <dookong59@gmail.com>
2026-05-31 09:57:05 +0900

    save: Solving Homework02

---
+++ Day0529_Homework02.cpp
@@ -0,0 +1,354 @@
+#include "Day0529_Homework02.h"
+
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+using namespace std;
+
+void Day0529_Homework02_Run()
+{
+	// 랜덤 시드 설정
+	srand(time(0));
+
+	// 카드 초기화
+	int CardPool[Day0529_CARD_MAX + 1] = { 0, };	// 각 카드는 4장씩 존재. 1~13번째만 사용 (0번째는 무조건 0)
+	for (int i = 1; i <= Day0529_CARD_MAX; i++)
+	{
+		CardPool[i] = 4;
+	}
+
+	// 게임 진행
+	Day0529_BlackJack_Play(CardPool);
+}
+
+// 블랙잭 게임 진행 함수
+void Day0529_BlackJack_Play(int* CardPool)
+{
+	// 딜러 및 플레이어 입장
+	CardPocket DealerCardPocket;
+	CardPocket PlayerCardPocket;
+
+	// 초기 배분: 플레이어, 딜러 각각 카드 2장씩 뽑기
+	int DealerHiddenCard = Day0529_GameReady(CardPool, DealerCardPocket, PlayerCardPocket);
+
+	bool bIsPlayerWin = false;			// 플레이어 승리 여부
+
+	bool bIsPlayerBurst = false;		// 플레이어 버스트 여부
+	bool bIsDealerBurst = false;		// 딜러 버스트 여부
+
+	// 블랙잭 확인
+	bool bIsPlayerBlackJack = false;	// 플레이어 블랙잭 여부
+	bool bIsDealerBlackJack = false;	// 딜러 블랙잭 여부
+	if (Day0529_CheckBlackJack(PlayerCardPocket) && Day0529_CheckBlackJack(DealerCardPocket))
+	{
+		bIsPlayerBlackJack = true;
+		bIsDealerBlackJack = true;
+	}
+	else if (Day0529_CheckBlackJack(PlayerCardPocket))
+	{
+		bIsPlayerBlackJack = true;
+		bIsPlayerWin = true;
+	}
+	else if (Day0529_CheckBlackJack(DealerCardPocket))
+	{
+		bIsDealerBlackJack = true;
+		bIsPlayerWin = false;
+	}
+	// 버스트 확인
+	bIsPlayerBurst = Day0529_CheckBurst(PlayerCardPocket);
+	bIsDealerBurst = Day0529_CheckBurst(DealerCardPocket);
+
+	bool bIsPlayerTurn = true;		// 카드 뽑기 턴 관리
+	bool bIsPlayerStand = false;	// 플레이어 스탠드 여부
+	// 턴 진행
+	if (!bIsPlayerBlackJack && !bIsDealerBlackJack && !bIsDealerBurst && !bIsPlayerBurst)
+	{
+		// 플레이어 턴 진행
+		bIsPlayerBurst = Day0529_PlayTurn(PlayerCardPocket, bIsPlayerTurn, CardPool);
+		if (bIsPlayerBurst)
+		{
+			// 플레이어 버스트 나지 않으면 딜러 턴 진행
+			bIsDealerBurst = Day0529_PlayTurn(DealerCardPocket, !bIsPlayerTurn, CardPool);
+		}
+	}
+
+	// 승패 판정
+	printf("게임이 종료됐습니다. 승패를 가립니다.\n딜러의 다른 카드는 %s였습니다.\n\n", Day0529_GetCardNumberName(DealerHiddenCard).c_str());
+	Day0529_DetermineWinner(PlayerCardPocket, DealerCardPocket, bIsPlayerBlackJack, bIsDealerBlackJack);
+}
+
+// 초기 배분. 딜러의 숨긴 카드 반환
+int Day0529_GameReady(int* CardPool, CardPocket& Dealer, CardPocket& Player)
+{
+	string DealerDraw[2] = { "", "" };
+	string PlayerDraw[2] = { "", "" };
+
+	// 플레이어와 딜러 각각 카드 두 장씩 뽑기
+	int TempCard = 0;
+	for (int i = 2; i; i--)
+	{
+		TempCard = Day0529_DrawCard(CardPool);
+		Player.Score_ExceptAce += (TempCard > 9 ? 10 : TempCard);
+		PlayerDraw[2 - i] = Day0529_GetCardNumberName(TempCard);
+
+		TempCard = Day0529_DrawCard(CardPool);
+		Dealer.Score_ExceptAce += (TempCard > 9 ? 10 : TempCard);
+		DealerDraw[2 - i] = Day0529_GetCardNumberName(TempCard);
+	}
+
+	// 플레이어: 카드 두 장 모두 공개
+	printf("플레이어가 뽑은 카드 번호: %s, %s\n", PlayerDraw[0].c_str(), PlayerDraw[1].c_str());
+
+	// 딜러: 카드 한 장만 공개
+	printf("딜러 오픈 카드: %s\n", DealerDraw[0].c_str());	// 처음 뽑은 카드를 오픈하기로 개발자가 임의로 정함
+
+	return TempCard;
+}
+
+// 카드 번호를 문자열로 변환하는 함수
+string Day0529_GetCardNumberName(int CardNumber)
+{
+	string Result = "";
+
+	if (CardNumber == 1)
+	{
+		Result = "Ace";
+	}
+	else if (CardNumber == 10)
+	{
+		Result = "10";
+	}
+	else if (CardNumber == 11)
+	{
+		Result = "Jack";
+	}
+	else if (CardNumber == 12)
+	{
+		Result = "Queen";
+	}
+	else if (CardNumber == 13)
+	{
+		Result = "King";
+	}
+	else
+	{
+		Result = '0' + CardNumber;	// 2~9
+	}
+
+	return Result;
+}
+
+// 카드 한 장 뽑기
+int Day0529_DrawCard(int* CardPool)
+{
+	int Result = 0;	// 뽑은 카드
+
+	if (Day0529_CheckCardPool(CardPool)) {
+		while (*(CardPool + (Result = rand() % Day0529_CARD_MAX + 1)) == 0);
+		(*(CardPool + Result))--;
+	}
+
+	return Result;
+}
+
+// 카드 남았는지 확인
+bool Day0529_CheckCardPool(int* CardPool)
+{
+	bool bIsPossible = false;
+	for (int i = 1; i <= Day0529_CARD_MAX; i++)
+	{
+		if (CardPool[i] != 0)
+		{
+			bIsPossible = true;
+		}
+	}
+
+	return bIsPossible;
+}
+
+// 턴 주인 안내
+void Day0529_PrintTurnOwner(bool bIsPlayerTurn)
+{
+	printf("지금은 %s의 턴입니다.\n", bIsPlayerTurn ? "플레이어" : "딜러");
+}
+
+// 플레이 턴: 카드 뽑고 히트, 스탠드 선택. 버스트 여부 반환
+bool Day0529_PlayTurn(CardPocket& InCardPocket, bool bIsPlayerTurn, int* CardPool)
+{
+	bool bIsBurst = false;	// 버스트 선택
+	int TempCard = 0;		// 뽑은 카드 임시 저장
+
+	// 턴 주인 안내
+	Day0529_PrintTurnOwner(bIsPlayerTurn);
+
+	// 플레이어 턴
+	int InputAction = 0;
+	if (bIsPlayerTurn)
+	{
+		bool bIsStand = false;	// 스탠드 선택
+		while (InCardPocket.TotalScore() < Day0529_WINNING_SCORE && (!bIsBurst && !bIsStand))
+		{
+			printf("\n1. 히트: 카드 한 장 더 뽑기\n2. 스탠드: 카드 뽑기 멈추기\n행동을 결정해주세요: ");
+			cin >> InputAction;
+			
+			if (InputAction == 1)
+			{
+				TempCard = Day0529_DrawCard(CardPool);
+				printf("뽑은 카드: %s\n", Day0529_GetCardNumberName(TempCard).c_str());
+				Day0529_GetCard(InCardPocket, TempCard);
+				bIsBurst = Day0529_CheckBurst(InCardPocket);
+			}
+			else if (InputAction == 2)
+			{
+				bIsStand = true;
+				printf("플레이어 턴 종료. 딜러가 카드를 뽑습니다.");
+			}
+			else
+			{
+				printf("잘못된 입력입니다. 다시 입력해주세요.");
+			}
+		}
+	}
+	// 딜러 턴
+	else
+	{
+		printf("Dealer: %d\n", InCardPocket.TotalScore());
+		while (InCardPocket.TotalScore() < Day0529_DEALER_SOFT_SCORE)
+		{
+			TempCard = Day0529_DrawCard(CardPool);
+			printf("뽑은 카드: %s\n", Day0529_GetCardNumberName(TempCard).c_str());
+			Day0529_GetCard(InCardPocket, TempCard);
+			printf("Dealer: %d\n", InCardPocket.TotalScore());
+		}
+		bIsBurst = Day0529_CheckBurst(InCardPocket);
+	}
+
+	return bIsBurst;
+}
+
+// 카드 주머니에 넣기
+void Day0529_GetCard(CardPocket& InCardPocket, int CardNumber)
+{
+	if (CardNumber == 1)
+	{
+		InCardPocket.AceCount++;
+	}
+	else if (CardNumber == 10 || CardNumber == 11 || CardNumber == 12 || CardNumber == 13)
+	{
+		InCardPocket.Score_ExceptAce += 10;
+	}
+	else
+	{
+		InCardPocket.Score_ExceptAce += CardNumber;
+	}
+}
+
+// 버스트 확인
+bool Day0529_CheckBurst(CardPocket& InCardPocket)
+{
+	return InCardPocket.TotalScore() > Day0529_WINNING_SCORE;
+}
+
+// 블랙잭인지 확인 (A + 10)
+bool Day0529_CheckBlackJack(CardPocket& InCardPocket)
+{
+	return (InCardPocket.TotalScore() == Day0529_WINNING_SCORE && InCardPocket.AceCount > 0);
+}
+
+// 승패 판정
+void Day0529_DetermineWinner(CardPocket& PlayerPocket, CardPocket& DealerPocket, bool bIsPlayerBlackJack, bool bIsDealerBlackJack)
+{
+	int Winner = -1;	// 0: 무승부, 1: 플레이어 승, 2: 딜러 승
+
+	int PlayerTotal = PlayerPocket.TotalScore();
+	int DealerTotal = DealerPocket.TotalScore();
+
+	// 플레이어가 블랙잭인 경우:  딜러도 블랙잭이어도 무조건 플레이어 승
+	if (bIsPlayerBlackJack)
+	{
+		Winner = 1;
+		printf("블랙잭!!!!!\n");
+	}
+	// 딜러만 블랙잭인 경우
+	else if (bIsDealerBlackJack)
+	{
+		Winner = 2;
+		printf("블랙잭!!!!!\n");
+	}
+	// 둘 다 블랙잭이 아닌 경우
+	else if (!bIsPlayerBlackJack && !bIsDealerBlackJack)
+	{
+		// 블랙잭이 아닌 경우
+		// 둘 다 버스트가 아닌 경우
+		if (PlayerTotal <= Day0529_WINNING_SCORE && DealerTotal <= Day0529_WINNING_SCORE)
+		{
+			if (PlayerTotal > DealerTotal)
+			{
+				Winner = 1;
+			}
+			else if (PlayerTotal < DealerTotal)
+			{
+				Winner = 2;
+			}
+			else
+			{
+				Winner = 0;
+			}
+		}
+		// 둘 중 하나라도 버스트인 경우
+		// 플레이어가 버스트면 무조건 딜러 승 (둘 다 버스트여도 플레이어가 버스트면 딜러 승)
+		// 딜러가 버스트면 무조건 플레이어 승
+		// 둘 중 하나라도 버스트
+		if (PlayerTotal > Day0529_WINNING_SCORE)
+		{
+			Winner = 2;
+		}
+		else if (DealerTotal > Day0529_WINNING_SCORE)
+		{
+			Winner = 1;
+		}
+		// 둘 다 버스트 안 당함
+		else if (PlayerTotal == DealerTotal)
+		{
+			Winner = 0;
+		}
+		else if (PlayerTotal > DealerTotal)
+		{
+			Winner = 1;
+		}
+		else if (PlayerTotal < DealerTotal)
+		{
+			Winner = 2;
+		}
+		else
+		{
+			Winner = -1;	// error
+		}
+	}
+
+	// 승패 결과 출력
+	printf("플레이어의 총점은 %d점, 딜러의 총점은 %d점입니다.\n", PlayerTotal, DealerTotal);
+	Day0529_PrintWinner(Winner);
+}
+void Day0529_PrintWinner(int Winner)
+{
+	string WinnerPhrase = "";
+	if (Winner == 0)
+	{
+		WinnerPhrase = "무승부입니다!!";
+	}
+	else if (Winner == 1)
+	{
+		WinnerPhrase = "플레이어가 이겼습니다!!";
+	}
+	else if (Winner == 2)
+	{
+		WinnerPhrase = "딜러가 이겼습니다!!";
+	}
+	else
+	{
+		WinnerPhrase = "Error!!!!!!!!!!";
+	}
+
+	printf("게임 결과는\n.\n.\n.\n%s\n수고하셨습니다.\n\n", WinnerPhrase.c_str());
+}
---
+++ Day0529_Homework02.h
@@ -0,0 +1,99 @@
+#pragma once
+
+#include <string>
+
+// 블랙잭 만들기
+// 게임 목표
+//   두 카드의 합이 21에 가깝게 만들되, 21을 넘지 않는 것이 목표
+//   21을 초과하면(버스트, Bust)  즉시 패배
+// 배팅은 없음
+// 카드의 값
+//   2~10: 카드 숫자 그대로 점수.
+//   J(잭), Q(퀸), K(킹) : 각각 10점.
+//   A(에이스) : 1점 또는 11점(유리한 쪽으로 자동 선택).
+// 게임 진행 절차
+//   초기 배분
+//     딜러와 플레이어 모두 2장씩 카드를 받음.
+//     플레이어의 카드는 두 장 모두 공개.
+//     딜러는 한 장은 공개(오픈 카드), 한 장은 비공개(홀 카드).
+//   플레이어 턴
+//     플레이어가 먼저 행동.
+//     선택지:
+//       Hit(히트) : 카드를 한 장 더 받음.
+//       Stand(스탠드) : 더 이상 카드를 받지 않고 멈춤.
+//     플레이어가 21을 초과하면(버스트) 즉시 패배.
+//   딜러 턴
+//     플레이어가 스탠드를 하면 딜러 차례.
+//     딜러는 자신의 카드 합이 17 이상이 될 때까지 계속 히트(카드 받기)해야 함.
+//       A가 포함된 "소프트 17"(A + 6 = 7 또는 17)도 멈춤.
+//     17 이상이면 멈추고, 21을 넘으면 딜러 패배.
+// 승패 판정
+//   버스트가 아닌 경우, 점수 비교.
+//     플레이어가 21에 더 가까우면 승리.
+//     동점이면 무승부(Push).
+//     딜러가 더 가까우면 패배.
+// 블랙잭: 처음 받은 두 장이 A + 10(또는 J, Q, K)이면 블랙잭.일반적으로 블랙잭이 단순 21점보다 우선함.
+
+const int Day0529_CARD_MAX = 13;	// 스페이드, 다이아몬드, 클로버, 하트가 각각 13장씩 존재
+const int Day0529_WINNING_SCORE = 21;
+const int Day0529_DEALER_SOFT_SCORE = 17;
+
+struct CardPocket
+{
+	int Score_ExceptAce = 0;	// 지금까지 뽑은 카드 점수 합산 (에이스 제외)
+	int AceCount = 0;	// 지금까지 뽑은 에이스 개수
+
+	// 에이스까지 합산한 총점
+	int TotalScore()
+	{
+		int Result = Score_ExceptAce;	// Ace 제외 카드들로 점수 초기화
+
+		// Ace 카드들 점수 결정
+		if (AceCount)
+		{
+			// 21점을 넘으면 바로 패배이기 때문에 Ace는 더했을 때 21점보다 작거나 같을 때만 11점을 가질 수 있다.
+			const int HighAce = 11;
+			const int LowAce = 1;
+			for (int i = 0; i < AceCount; i++)
+			{
+				if (Result + HighAce > Day0529_WINNING_SCORE)
+				{
+					Result += LowAce;
+				}
+				else
+				{
+					Result += HighAce;
+				}
+			}
+		}
+
+		return Result;
+	}
+};
+
+void Day0529_Homework02_Run();
+
+void Day0529_BlackJack_Play(int* CardPool);
+
+int Day0529_GameReady(int* CardPool, CardPocket& Dealer, CardPocket& Player);
+
+std::string Day0529_GetCardNumberName(int CardNumber);
+
+int Day0529_DrawCard(int* CardPool);
+
+bool Day0529_CheckCardPool(int* CardPool);
+
+void Day0529_PrintTurnOwner(bool bIsPlayerTurn);
+
+bool Day0529_PlayTurn(CardPocket& InCardPocket, bool bIsPlayerTurn, int* CardPool);
+
+void Day0529_GetCard(CardPocket& InCardPocket, int CardNumber);
+
+bool Day0529_CheckBurst(CardPocket& InCardPocket);
+
+bool Day0529_CheckBlackJack(CardPocket& InCardPocket);
+
+void Day0529_DetermineWinner(CardPocket& PlayerPocket, CardPocket& DealerPocket, bool bIsPlayerBlackJack, bool bIsDealerBlackJack);
+
+void Day0529_PrintWinner(int Winner);
+
